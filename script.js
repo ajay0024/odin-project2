1b7ec08f2d9019791d31e14caae5cbab3df89db2
Ajay Pal <pythontest0024@gmail.com>
2022-12-12 13:39:04 +0530

    Add the functionality of final winner

--- script.js
+++ script.js
@@ -6,24 +6,32 @@ function getComputerChoice(){
 function playSingleRound(playerSelection, computerSelection){
     let possibleChoices = ["rock", "paper","scissors"] ;
     if (!possibleChoices.includes(playerSelection.toLowerCase())){
-        return (`You chose wrong word. Your choices are ${possibleChoices.join(", ")}`)
+        console.log (`You chose wrong word. Your choices are ${possibleChoices.join(", ")}`)
+        return ("Draw")
     }
     let playerIndex = possibleChoices.indexOf(playerSelection.toLowerCase())
     let computerIndex = possibleChoices. indexOf(computerSelection.toLowerCase())
     console.log(playerIndex, computerIndex)
     if (computerIndex == (playerIndex + 1)% possibleChoices.length){
-        return (`You Lose! ${computerSelection} beats ${playerSelection}`)
+        console.log (`You Lose! ${computerSelection} beats ${playerSelection}`);
+        return("Computer")
     } else if (playerIndex == (computerIndex + 1)% possibleChoices.length){
-        return (`You Win! ${playerSelection} beats ${computerSelection}`)
+        console.log (`You Win! ${playerSelection} beats ${computerSelection}`);
+        return ("Player")
     } else {
-        return (`It was a Draw! Both chose ${playerSelection}`)
+        console.log (`It was a Draw! Both chose ${playerSelection}`);
+        return ("Draw")
     }
 }
 function game(){
+    let playerWinningCount = 0;
+    let computerWinningCount =0
     for (let i = 0; i < 5; i++) {
-        playerChoice=prompt("Enter Your Choice From 'Rock', 'Paper', 'Scissors'. Results are declared in console.")
-        computerChoice = getComputerChoice()
-        console.log(playSingleRound(playerChoice,computerChoice))
+        let playerChoice=prompt(`Round ${i+1} : Enter Your Choice From 'Rock', 'Paper', 'Scissors'. Results are declared in console.`)
+        let computerChoice = getComputerChoice();
+        let roundWinner = playSingleRound(playerChoice,computerChoice);
+        (roundWinner == "Player")? playerWinningCount++ : (roundWinner == "Computer") && computerWinningCount++ ;
     }
+    console.log ( (playerWinningCount > computerWinningCount)? "You Won!" : (computerWinningCount > playerWinningCount) ? "Computer Won!": "It was a Draw")
 }
 game()
