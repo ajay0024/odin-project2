ff4afc8a9f5c61175940f1d18f69892ba3a82252
ajay0024 <pythontest0024@gmail.com>
2022-12-20 18:20:35 +0530

    complete game and remove extra content

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
+const WINNINGCONDITION = 5;
+
 function getComputerChoice() {
     let possibleResults = ["Rock", "Paper", "Scissors"];
     let randomNumber = Math.floor(Math.random() * possibleResults.length);
     return (possibleResults[randomNumber]);
 }
 function playSingleRound(playerSelection, computerSelection) {
-    
+
     let winner;
     let possibleChoices = ["rock", "paper", "scissors"];
-    
+
     let playerIndex = possibleChoices.indexOf(playerSelection.toLowerCase())
     let computerIndex = possibleChoices.indexOf(computerSelection.toLowerCase())
-    
+
     if (computerIndex == (playerIndex + 1) % possibleChoices.length) {
         console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-        result = "win"
+        result = "lose"
     } else if (playerIndex == (computerIndex + 1) % possibleChoices.length) {
         console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
-        result = "lose"
+        result = "win"
     } else {
         console.log(`It was a Draw! Both chose ${playerSelection}`);
-        winner = "draw"
+        result = "draw"
     }
 
-    displayRoundPlay(playerSelection,computerSelection, winner);
-    return winner
+    updateScore(result);
+    displayRoundPlay(playerSelection, computerSelection, result);
+    
+    return result
 }
-function displayRoundPlay(a,b,winner){
+function displayRoundPlay(a, b, winner) {
     let gameNode = document.getElementById("game");
     let playerButton = document.getElementById(a);
     let computerButton = document.getElementById(b.toLowerCase());
-    let resultText= `${winner == "draw" ? "It's Draw!": winner == "win" ?"You Win!" : "You Lose!"}`
+    let resultText = `${winner == "draw" ? "It's Draw!" : winner == "win" ? "You Win!" : "You Lose!"}`
     console.log(resultText)
-    
+
 
     let parent = gameNode.parentNode;
     parent.removeChild(gameNode);
@@ -46,12 +50,26 @@ function displayRoundPlay(a,b,winner){
     tempNode.appendChild(playerButton.cloneNode(true));
     newNode.appendChild(tempNode);
 
+    let playerScore = document.getElementById("player-score");
+    let computerScore = document.getElementById("computer-score");
+    let newPlayerScore = parseInt(playerScore.innerHTML);
+    let newComputerScore = parseInt(computerScore.innerHTML);
+  
+    if (newPlayerScore == WINNINGCONDITION ) resultText = "You Won the Game!";
+    if (newComputerScore == WINNINGCONDITION) resultText = "You Lost the Game!"
+    let buttontext = "Play Next";
+    let gameOver = false;
+    if (newPlayerScore == WINNINGCONDITION || newComputerScore == WINNINGCONDITION){
+        buttontext = "Restart Game?";
+        gameOver = true;
+    }
+
     let resultNode = document.createElement("div");
-    resultNode.setAttribute("id","resultText")
+    resultNode.setAttribute("id", "resultText")
     resultNode.innerHTML += `<p>${resultText}</p>`;
     let playAgainButtonNode = document.createElement('button');
-    playAgainButtonNode.textContent = "Play Again";
-    playAgainButtonNode.addEventListener("click",()=> replaceNode(newNode, gameNode))
+    playAgainButtonNode.textContent = buttontext;
+    playAgainButtonNode.addEventListener("click", () => replaceNode(newNode, gameNode, gameOver))
     resultNode.appendChild(playAgainButtonNode);
     newNode.appendChild(resultNode);
 
@@ -66,22 +84,43 @@ function displayRoundPlay(a,b,winner){
     parent.appendChild(newNode);
 
 }
-function replaceNode(x,y){
-    document.body.replaceChild(y,x);
-    
-}
-function game() {
-    let playerWinningCount = 0;
-    let computerWinningCount = 0
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = prompt(`Round ${i + 1} : Enter Your Choice From 'Rock', 'Paper', 'Scissors'. Results are declared in console.`)
-        let computerChoice = getComputerChoice();
-        let roundWinner = playSingleRound(playerChoice, computerChoice);
-        (roundWinner == "Player") ? playerWinningCount++ : (roundWinner == "Computer") && computerWinningCount++;
+function replaceNode(x, y, gameOver) {
+    if (gameOver){
+        document.getElementById("player-score").innerHTML = 0;
+        document.getElementById("computer-score").innerHTML = 0;
     }
-    console.log((playerWinningCount > computerWinningCount) ? "You Won!" : (computerWinningCount > playerWinningCount) ? "Computer Won!" : "It was a Draw")
+    document.body.replaceChild(y, x);
 }
-// game()
+function updateScore(didWin) {
+    let playerScore = document.getElementById("player-score");
+    let computerScore = document.getElementById("computer-score");
+    console.log(playerScore, computerScore, didWin)
+    if (didWin == "win") {
+        playerScore.innerHTML = parseInt(playerScore.innerHTML) + 1;
+    } else if (didWin == "lose") {
+        computerScore.innerHTML = parseInt(computerScore.innerHTML) + 1;
+    } 
+}
+
+document.querySelector("#rules button").addEventListener("click",()=>{
+    document.getElementById("overlay").classList.add("hide")
+})
+document.querySelector("#show-rules").addEventListener("click",()=>{
+    document.getElementById("overlay").classList.remove("hide")
+})
+
+// function game() {
+//     let playerWinningCount = 0;
+//     let computerWinningCount = 0
+//     for (let i = 0; i < 5; i++) {
+//         let playerChoice = prompt(`Round ${i + 1} : Enter Your Choice From 'Rock', 'Paper', 'Scissors'. Results are declared in console.`)
+//         let computerChoice = getComputerChoice();
+//         let roundWinner = playSingleRound(playerChoice, computerChoice);
+//         (roundWinner == "Player") ? playerWinningCount++ : (roundWinner == "Computer") && computerWinningCount++;
+//     }
+//     console.log((playerWinningCount > computerWinningCount) ? "You Won!" : (computerWinningCount > playerWinningCount) ? "Computer Won!" : "It was a Draw")
+// }
+// // game()
 
 let optionButtons = document.getElementsByClassName('option-button');
 console.log(optionButtons);
