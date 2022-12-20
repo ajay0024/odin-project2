a0431827bd96ce399a0a74f54d11733ecb95168b
ajay0024 <pythontest0024@gmail.com>
2022-12-20 16:52:48 +0530

    working UI

--- script.js
+++ script.js
@@ -1,37 +1,96 @@
-function getComputerChoice(){
-    let possibleResults = ["Rock", "Paper","Scissors"] ;
-    let randomNumber=Math.floor(Math.random()*possibleResults.length);
+function getComputerChoice() {
+    let possibleResults = ["Rock", "Paper", "Scissors"];
+    let randomNumber = Math.floor(Math.random() * possibleResults.length);
     return (possibleResults[randomNumber]);
 }
-function playSingleRound(playerSelection, computerSelection){
-    let possibleChoices = ["rock", "paper","scissors"] ;
-    if (!possibleChoices.includes(playerSelection.toLowerCase())){
-        console.log (`You chose wrong word. Your choices are ${possibleChoices.join(", ")}`)
-        return ("Draw")
-    }
+function playSingleRound(playerSelection, computerSelection) {
+    
+    let winner;
+    let possibleChoices = ["rock", "paper", "scissors"];
+    
     let playerIndex = possibleChoices.indexOf(playerSelection.toLowerCase())
-    let computerIndex = possibleChoices. indexOf(computerSelection.toLowerCase())
-    console.log(playerIndex, computerIndex)
-    if (computerIndex == (playerIndex + 1)% possibleChoices.length){
-        console.log (`You Lose! ${computerSelection} beats ${playerSelection}`);
-        return("Computer")
-    } else if (playerIndex == (computerIndex + 1)% possibleChoices.length){
-        console.log (`You Win! ${playerSelection} beats ${computerSelection}`);
-        return ("Player")
+    let computerIndex = possibleChoices.indexOf(computerSelection.toLowerCase())
+    
+    if (computerIndex == (playerIndex + 1) % possibleChoices.length) {
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+        result = "win"
+    } else if (playerIndex == (computerIndex + 1) % possibleChoices.length) {
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+        result = "lose"
     } else {
-        console.log (`It was a Draw! Both chose ${playerSelection}`);
-        return ("Draw")
+        console.log(`It was a Draw! Both chose ${playerSelection}`);
+        winner = "draw"
     }
+
+    displayRoundPlay(playerSelection,computerSelection, winner);
+    return winner
 }
-function game(){
+function displayRoundPlay(a,b,winner){
+    let gameNode = document.getElementById("game");
+    let playerButton = document.getElementById(a);
+    let computerButton = document.getElementById(b.toLowerCase());
+    let resultText= `${winner == "draw" ? "It's Draw!": winner == "win" ?"You Win!" : "You Lose!"}`
+    console.log(resultText)
+    
+
+    let parent = gameNode.parentNode;
+    parent.removeChild(gameNode);
+    let newNode = document.createElement("div");
+    newNode.setAttribute("id", "currentPlay");
+
+
+    let tempNode = document.createElement("div");
+    let text1Node = document.createElement("p");
+    text1Node.textContent = "YOUR PICK";
+    tempNode.appendChild(text1Node);
+    tempNode.appendChild(playerButton.cloneNode(true));
+    newNode.appendChild(tempNode);
+
+    let resultNode = document.createElement("div");
+    resultNode.setAttribute("id","resultText")
+    resultNode.innerHTML += `<p>${resultText}</p>`;
+    let playAgainButtonNode = document.createElement('button');
+    playAgainButtonNode.textContent = "Play Again";
+    playAgainButtonNode.addEventListener("click",()=> replaceNode(newNode, gameNode))
+    resultNode.appendChild(playAgainButtonNode);
+    newNode.appendChild(resultNode);
+
+
+    tempNode = document.createElement("div");
+    let text2Node = document.createElement("p");
+    text2Node.textContent = "COMPUTER'S PICK";
+    tempNode.appendChild(text2Node);
+    tempNode.appendChild(computerButton.cloneNode(true))
+    newNode.appendChild(tempNode);
+
+    parent.appendChild(newNode);
+
+}
+function replaceNode(x,y){
+    document.body.replaceChild(y,x);
+    
+}
+function game() {
     let playerWinningCount = 0;
-    let computerWinningCount =0
+    let computerWinningCount = 0
     for (let i = 0; i < 5; i++) {
-        let playerChoice=prompt(`Round ${i+1} : Enter Your Choice From 'Rock', 'Paper', 'Scissors'. Results are declared in console.`)
+        let playerChoice = prompt(`Round ${i + 1} : Enter Your Choice From 'Rock', 'Paper', 'Scissors'. Results are declared in console.`)
         let computerChoice = getComputerChoice();
-        let roundWinner = playSingleRound(playerChoice,computerChoice);
-        (roundWinner == "Player")? playerWinningCount++ : (roundWinner == "Computer") && computerWinningCount++ ;
+        let roundWinner = playSingleRound(playerChoice, computerChoice);
+        (roundWinner == "Player") ? playerWinningCount++ : (roundWinner == "Computer") && computerWinningCount++;
     }
-    console.log ( (playerWinningCount > computerWinningCount)? "You Won!" : (computerWinningCount > playerWinningCount) ? "Computer Won!": "It was a Draw")
+    console.log((playerWinningCount > computerWinningCount) ? "You Won!" : (computerWinningCount > playerWinningCount) ? "Computer Won!" : "It was a Draw")
 }
-game()
+// game()
+
+let optionButtons = document.getElementsByClassName('option-button');
+console.log(optionButtons);
+[...optionButtons].forEach(btn => {
+    console.log(btn);
+    btn.addEventListener("click", e => {
+        let playerChoice = btn.getAttribute('id');
+        let computerChoice = getComputerChoice();
+        let roundWinner = playSingleRound(playerChoice, computerChoice);
+    })
+});
+
